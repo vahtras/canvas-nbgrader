e34a97e7dcb35faf691511130985332d556833d9
Olav Vahtras <vahtras@kth.se>
2020-05-18 22:31:19 +0200

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="canvas-nbgrader",
-    version="0.0.2",
+    version="0.0.3",
     author="Olav Vahtras",
     author_email="vahtras@kth.se",
     py_modules=["cnb", "util"],
